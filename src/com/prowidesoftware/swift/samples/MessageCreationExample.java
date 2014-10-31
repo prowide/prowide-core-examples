5dad98479a98d66000dc4df2a59fd9add1364a8d
Miguel Griffa <miguel@prowidesoftware.com>
2014-10-31 07:16:59 -0300

    API update for chained setters and obsolete setMessageType

--- src/com/prowidesoftware/swift/samples/MessageCreationExample.java
+++ src/com/prowidesoftware/swift/samples/MessageCreationExample.java
@@ -6,7 +6,6 @@ package com.prowidesoftware.swift.samples;
 import java.util.Calendar;
 
 import com.prowidesoftware.swift.io.parser.SwiftParser;
-import com.prowidesoftware.swift.model.SwiftMessage;
 import com.prowidesoftware.swift.model.field.Field20;
 import com.prowidesoftware.swift.model.field.Field23B;
 import com.prowidesoftware.swift.model.field.Field32A;
@@ -16,89 +15,85 @@ import com.prowidesoftware.swift.model.field.Field71A;
 import com.prowidesoftware.swift.model.mt.mt1xx.MT103;
 
 /**
- * Example of message creation using high level API from WIFE.
- * An MT103 is created and converted to the SWIFT FIN format.
+ * Example of message creation using high level API from WIFE. An MT103 is
+ * created and converted to the SWIFT FIN format.
  * 
+ * Running this program produces the following SWIFT FIN message content:
+<pre>
+{1:F01FOOSEDR0AXXX0000000000}{2:I103FOORECV0XXXXN}{4:
+:20:REFERENCE
+:23B:CRED
+:32A:141031EUR1234567,89
+:50A:/12345678901234567890
+FOOBANKXXXXX
+:59:/12345678901234567890
+JOE DOE
+:71A:OUR
+-}
+</pre>
  * @author www.prowidesoftware.com
  */
 public class MessageCreationExample {
 
-	/**
-	 * This example creates a new MT103 using MT and Field helper classes.
+    /**
+     * This example creates a new MT103 using MT and Field helper classes.
+     */
+    public static void main(String[] args) throws Exception {
+	/*
+	 * Create the MT class, it will be initialized as an outgoing message
+	 * with normal priority
 	 */
-	public static void main(String[] args) throws  Exception {
-		/*
-		 * Create the MT class, it will be initialized as an outgoing message with normal priority
-		 */
-		final MT103 m = new MT103(new SwiftMessage(true));
-		
-		/*
-		 * Set sender and receiver BIC codes 
-		 */
-		m.setSender("FOOSEDR0AXXX");
-		m.setReceiver("FOORECV0XXXX");
+	final MT103 m = new MT103();
 
-        // Set the message type on the model message passed in constructor
-        m.getSwiftMessage().getBlock2().setMessageType("103");
+	/*
+	 * Set sender and receiver BIC codes
+	 */
+	m.setSender("FOOSEDR0AXXX");
+	m.setReceiver("FOORECV0XXXX");
 
-		/*
-		 * Start adding the message's fields in correct order
-		 */
-		m.addField(new Field20("REFERENCE"));
-		m.addField(new Field23B("CRED"));
+	/*
+	 * Start adding the message's fields in correct order
+	 */
+	m.addField(new Field20("REFERENCE"));
+	m.addField(new Field23B("CRED"));
 
-		/*
-		 * Add a field using comprehensive setters API
-		 */
-		Field32A f32A = new Field32A();
-		f32A.setDate(Calendar.getInstance());
-		f32A.setCurrency("EUR");
-		f32A.setAmount("1234567,89");
-		m.addField(f32A);
+	/*
+	 * Add a field using comprehensive setters API
+	 */
+	Field32A f32A = new Field32A()
+		.setDate(Calendar.getInstance())
+		.setCurrency("EUR")
+		.setAmount("1234567,89");
+	m.addField(f32A);
 
-		/*
-		 * Add the orderer field
-		 */
-		Field50A f50A = new Field50A();
-		f50A.setAccount("12345678901234567890");
-		f50A.setBIC("FOOBANKXXXXX");
-		m.addField(f50A);
-		
-		/*
-		 * Add the beneficiary field
-		 */
-		Field59 f59 = new Field59();
-		f59.setAccount("12345678901234567890");
-		f59.setNameAndAddress("JOE DOE");
-		m.addField(f59);
+	/*
+	 * Add the orderer field
+	 */
+	Field50A f50A = new Field50A()
+		.setAccount("12345678901234567890")
+		.setBIC("FOOBANKXXXXX");
+	m.addField(f50A);
 
-		/*
-		 * Add the commission indication
-		 */
-		m.addField(new Field71A("OUR"));
-		
-		/*
-		 * Create and print out the SWIFT FIN message string
-		 */
-		System.out.println(m.FIN());
-		
-		/*
-		 * Below text is the expected SWIFT FIN message content:
-		 * 
-		 * {1:F01FOOSEDR0AXXX0000000000}{2:IFOORECV0XXXXN}{4:
-		 * :20:REFERENCE
-		 * :23B:CRED
-		 * :32A:1303EUR1234567,89
-		 * :50A:/12345678901234567890
-		 * FOOBANKXXXXX
-		 * :59:/12345678901234567890
-		 * JOE DOE
-		 * :71A:OUR
-		 * -}
-		 * 
-		 */
+	/*
+	 * Add the beneficiary field
+	 */
+	Field59 f59 = new Field59()
+		.setAccount("12345678901234567890")
+		.setNameAndAddress("JOE DOE");
+	m.addField(f59);
 
-        // Check the created message is parsaeble, notice it does not imply it is valid
-        new SwiftParser(m.FIN()).message();
-	}
+	/*
+	 * Add the commission indication
+	 */
+	m.addField(new Field71A("OUR"));
+
+	/*
+	 * Create and print out the SWIFT FIN message string
+	 */
+	System.out.println(m.FIN());
+
+	// Check the created message is parsaeble, notice it does not imply it
+	// is valid
+	new SwiftParser(m.FIN()).message();
+    }
 }

f554369f05d3f5e07a18d10d6c5d83313e10437b
sebastian <sebastian@prowidesoftware.com>
2016-09-29 16:20:57 -0300

    copyright notice

--- src/com/prowidesoftware/swift/samples/core/MessageCreation3Example.java
+++ src/com/prowidesoftware/swift/samples/core/MessageCreation3Example.java
@@ -1,6 +1,17 @@
-/*
- * Copyright (c) http://www.prowidesoftware.com/, 2014. All rights reserved.
- */
+/*******************************************************************************
+ * Copyright (c) 2016 Prowide Inc.
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Lesser General Public License as 
+ *     published by the Free Software Foundation, either version 3 of the 
+ *     License, or (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+ *     
+ *     Check the LGPL at <http://www.gnu.org/licenses/> for more details.
+ *******************************************************************************/
 package com.prowidesoftware.swift.samples.core;
 
 import java.util.Calendar;
--- src/com/prowidesoftware/swift/samples/core/MessageToJsonExample.java
+++ src/com/prowidesoftware/swift/samples/core/MessageToJsonExample.java
@@ -1,14 +1,22 @@
-/*
- * Copyright (c) http://www.prowidesoftware.com/, 2013. All rights reserved.
- */
+/*******************************************************************************
+ * Copyright (c) 2016 Prowide Inc.
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Lesser General Public License as 
+ *     published by the Free Software Foundation, either version 3 of the 
+ *     License, or (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+ *     
+ *     Check the LGPL at <http://www.gnu.org/licenses/> for more details.
+ *******************************************************************************/
 package com.prowidesoftware.swift.samples.core;
 
 import java.io.IOException;
-import java.io.StringWriter;
 
-import com.prowidesoftware.swift.io.ConversionService;
 import com.prowidesoftware.swift.io.parser.SwiftParser;
-import com.prowidesoftware.swift.io.writer.XMLWriterVisitor;
 import com.prowidesoftware.swift.model.SwiftMessage;
 
 /**
